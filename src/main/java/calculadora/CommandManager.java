a26e4547b0f77b37d41239f593e4da76305a9d3b
dsmontoro <dsm@alumnos.upm.es>
2015-10-10 12:06:51 +0200

    Creación calculadora

---
+++ src/main/java/calculadora/CommandManager.java
@@ -0,0 +1,22 @@
+package calculadora;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import calculadora.Comando;
+
+public class CommandManager {
+    private Map<String, Comando> comandos = new HashMap<>();
+
+    public void add(Comando comando) {
+        this.comandos.put(comando.name(), comando);
+    }
+
+    public void execute(String key) {
+        this.comandos.get(key).execute();
+    }
+
+    public String[] keys() {
+        return this.comandos.keySet().toArray(new String[0]);
+    }
+}
---
+++ src/main/java/calculadora/MainCalculator.java
@@ -0,0 +1,29 @@
+package calculadora;
+
+import calculadora.PrintCommand;
+import calculadora.ResetCommand;
+import calculadora.SubtractCommand;
+import calculadora.AddCommand;
+import upm.jbb.IO;
+
+public class MainCalculator {
+    private CommandManager commandManager;
+
+    public MainCalculator() {
+        Calculator calculator = new Calculator();
+        this.commandManager = new CommandManager();
+        this.commandManager.add(new AddCommand(calculator));
+        this.commandManager.add(new SubtractCommand(calculator));
+        this.commandManager.add(new ResetCommand(calculator));
+        this.commandManager.add(new PrintCommand(calculator));
+    }
+
+    public void execute() {
+        String key = (String) IO.getIO().select(this.commandManager.keys());
+        this.commandManager.execute(key);
+    }
+
+    public static void main(String[] args) {
+        IO.getIO().addView(new MainCalculator());
+    }
+}
